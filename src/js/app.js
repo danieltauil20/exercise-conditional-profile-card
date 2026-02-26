d277e4c94e26b1b97e4731bf69165a522c4776ae
Jose Daniel Tauil Wozniak <Danieltauil@hotmail.com>
2026-02-26 20:53:55 +0000

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -28,14 +28,20 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  if (variables.name == null) variables.name = "Lucy";
+  if (variables.lastName == null) variables.lastName = "Boilett";
+  if (variables.role == null) variables.role = "Web Developer";
+  if (variables.country == null) variables.country = "Miami";
+  if (variables.city == null) variables.city = "USA";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
+  
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name} ${variables.lastName}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.country}, ${variables.city}</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
